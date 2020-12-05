d60496a4f3074b6def6752c4bc786f16a5afca76
Dan Rusei <dan.rusei@gmail.com>
2020-12-05 21:28:16 +0200

    insert mode

--- linkedlist/insert_node/main.go
+++ linkedlist/insert_node/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type node struct {
 	val  int
 	Next *node
@@ -10,22 +12,63 @@ type linkedlist struct {
 	Head   *node
 }
 
-//CreateList defines new linked list
-func NewLinkedlist() *linkedlist {
+func (ll *linkedlist) String() string {
+	cur := ll.Head
+	printedlist := fmt.Sprintf("%d", cur.val)
+	for ; cur.Next != nil; cur = cur.Next {
+		printedlist = printedlist + fmt.Sprintf("%d", cur.val)
+	}
+	return printedlist
+
+}
+
+func newLinkedlist() *linkedlist {
 	return &linkedlist{}
 }
 
-func NewNode(val int) *node {
+func newNode(val int) *node {
 	return &node{val, nil}
 }
 
-func (llist *linkedlist) sortedInsert(node *node) {
+func (ll *linkedlist) sortedInsert(n *node) {
+	if ll.Head == nil {
+		ll.Head = n
+		ll.length++
+		return
+	}
+	if ll.Head.val > n.val {
+		n.Next = ll.Head
+		ll.Head = n
+		ll.length++
+	}
 
+	cur := ll.Head
+	for cur.val > n.val {
+		if cur.Next == nil {
+			cur.Next = n
+			ll.length++
+			return
+		}
+		cur = cur.Next
+	}
+	n.Next = cur.Next
+	cur.Next = n
 }
 
 func main() {
-	llist := NewLinkedlist()
-	newNode := NewNode(5)
-	llist.sortedInsert(newNode)
+	llist := newLinkedlist()
+	node := newNode(5)
+	llist.sortedInsert(node)
+	node = newNode(10)
+	llist.sortedInsert(node)
+	node = newNode(7)
+	llist.sortedInsert(node)
+	node = newNode(3)
+	llist.sortedInsert(node)
+	node = newNode(1)
+	llist.sortedInsert(node)
+	node = newNode(9)
+	llist.sortedInsert(node)
 
+	fmt.Println(llist)
 }
