f5d7eaec4f8eed8205fabebce503dc8694a8df04
Dan Rusei <dan.rusei@gmail.com>
2020-12-05 19:14:01 +0200

    working on linkedlist

--- linkedlist/insert_node/main.go
+++ linkedlist/insert_node/main.go
@@ -1,10 +1,31 @@
 package main
 
-import (
-linkedlist "github.com/danrusei/algorithms_with_Go/blob/main/linked_list"
-)
+type node struct {
+	val  int
+	Next *node
+}
 
+type linkedlist struct {
+	length int
+	Head   *node
+}
+
+//CreateList defines new linked list
+func NewLinkedlist() *linkedlist {
+	return &linkedlist{}
+}
+
+func NewNode(val int) *node {
+	return &node{val, nil}
+}
+
+func (llist *linkedlist) sortedInsert(node *node) {
+
+}
 
 func main() {
+	llist := NewLinkedlist()
+	newNode := NewNode(5)
+	llist.sortedInsert(newNode)
 
 }
